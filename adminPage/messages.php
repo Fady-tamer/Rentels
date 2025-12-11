1c117d6f4819c0d065818de891aee05eb8924b9e
Eslam mohamed <eslamamaar7414@gmail.com>
2025-12-11 20:51:30 +0200

    إضافة صفحة الرسائل (message.html) لعرض المنتجات المهتم بها المستخدمون

---
+++ adminPage/messages.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Messages</title>
+</head>
+
+<body>
+
+    <div class="page-container">
+
+        <div class="title-box">
+            <h2 class="page-title">Messages</h2>
+        </div>
+
+        <div class="messages-container">
+
+            <div class="message-card">
+
+                <div class="msg-img-box">
+                    <img src="images/house1.jpg" alt="Product" class="msg-img">
+                </div>
+
+                <div class="msg-info">
+
+                    <div class="msg-product-name">
+                        <span class="msg-label">Product:</span>
+                        <span class="msg-value">Apartment in Cairo</span>
+                    </div>
+
+                </div>
+
+                <div class="msg-actions">
+                    <a href="#" class="msg-btn">View Details</a>
+                </div>
+
+            </div>
+
+            <div class="message-card">
+
+                <div class="msg-img-box">
+                    <img src="images/house2.jpg" alt="Product" class="msg-img">
+                </div>
+
+                <div class="msg-info">
+
+                    <div class="msg-product-name">
+                        <span class="msg-label">Product:</span>
+                        <span class="msg-value">Villa in Alexandria</span>
+                    </div>
+
+                </div>
+
+                <div class="msg-actions">
+                    <a href="#" class="msg-btn">View Details</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</body>
+</html>
