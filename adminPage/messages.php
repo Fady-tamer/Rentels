4d1aeadd44ec0a7b60b92fb971aa80387bfcd51f
Fady-tamer <fadytamer2018@gmail.com>
2025-12-11 21:00:07 +0200

    add messages style file

--- adminPage/messages.php
+++ adminPage/messages.php
@@ -1,65 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <title>Messages</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rentals - Messages</title>
+    <link rel="stylesheet" href="messages.css">
+    <link rel="icon" href="../img/icon wbg.ico" type="image/x-icon">
 </head>
 
 <body>
 
     <div class="page-container">
-
         <div class="title-box">
             <h2 class="page-title">Messages</h2>
         </div>
-
         <div class="messages-container">
-
             <div class="message-card">
-
                 <div class="msg-img-box">
                     <img src="images/house1.jpg" alt="Product" class="msg-img">
                 </div>
-
                 <div class="msg-info">
-
                     <div class="msg-product-name">
                         <span class="msg-label">Product:</span>
                         <span class="msg-value">Apartment in Cairo</span>
                     </div>
-
                 </div>
-
                 <div class="msg-actions">
                     <a href="#" class="msg-btn">View Details</a>
                 </div>
-
             </div>
-
-            <div class="message-card">
-
-                <div class="msg-img-box">
-                    <img src="images/house2.jpg" alt="Product" class="msg-img">
-                </div>
-
-                <div class="msg-info">
-
-                    <div class="msg-product-name">
-                        <span class="msg-label">Product:</span>
-                        <span class="msg-value">Villa in Alexandria</span>
-                    </div>
-
-                </div>
-
-                <div class="msg-actions">
-                    <a href="#" class="msg-btn">View Details</a>
-                </div>
-
-            </div>
-
         </div>
-
     </div>
 
 </body>
+
 </html>
